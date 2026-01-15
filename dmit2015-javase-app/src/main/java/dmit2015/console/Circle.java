8cd7266e1996d610012373d609eea13e12efacc8
Bilinskyi Nazar <brratyxa009@gmail.com>
2026-01-14 17:57:51 -0700

    [01 - First project] - Creating a project and a class

---
+++ dmit2015-javase-app/src/main/java/dmit2015/console/Circle.java
@@ -0,0 +1,22 @@
+package dmit2015.console;
+
+/**
+ * This class models a Circle shape.
+ *
+ * @author Nazar Bilinskyi
+ * @version 2026.01.14
+ */
+
+public class Circle {
+    // defining a field
+    private double radius;
+    //defining getters and setters (fields)
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
